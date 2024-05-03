089b7d6c95ab1f8169ede62f45c3a703612b5aa2
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-03 11:10:43 +0530

    socket handler

--- src/main/java/org/pmj/ClientHandler.java
+++ src/main/java/org/pmj/ClientHandler.java
@@ -38,4 +38,40 @@ public class ClientHandler implements Runnable{
             }
         }
     }
+
+    public void broadcastMessage(String messageToSend){
+        for (ClientHandler clientHandler : clientHandlers){
+            try {
+               if (!clientHandler.clientUsername.equals(clientUsername)){
+                   clientHandler.bufferedWriter.write(messageToSend );
+                     clientHandler.bufferedWriter.newLine();
+                   clientHandler.bufferedWriter.flush();
+               }
+            } catch (IOException e) {
+                closeEverything(clientHandler.socket, clientHandler.bufferedReader, clientHandler.bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler(){
+        clientHandlers.remove(this);
+        broadcastMessage("Player " + clientUsername + " is disconnected");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+       removeClientHandler();
+       try{
+           if (bufferedReader != null){
+               bufferedReader.close();
+           }
+           if (bufferedWriter != null){
+               bufferedWriter.close();
+           }
+           if (socket != null){
+               socket.close();
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
 }
